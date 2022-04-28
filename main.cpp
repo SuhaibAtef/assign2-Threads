3db5b6d77af7b54ad1dce5a5bbd7ef5745cce3f3
Suhaib Atef <suhaibatef20@gmail.com>
2022-04-28 17:36:47 +0300

    no-threading

--- main.cpp
+++ main.cpp
@@ -1,18 +1,25 @@
 #include "bits/stdc++.h"
 
 using namespace std;
-// numOfPrimes&&TotalNums&&PrimeList
+int numOfPrimes=0,totalNums=0;
+vector <int> primeList;
 /*
 
 When any of the threads starts executing, it will print its number (0 to T-1), and then the range of
     numbers that it is operating on. 
     
 */
+bool isPrime(int num){
+    for(int i=2;i<num;i++){
+        if(num%i==0)return false;
+    }
+    return true;
+}
+
 void write(string s) {
         fstream file;
-        file.open("out.txt", ios::out);
+        file.open("out.txt", ios_base::app);
         file << s << endl;
-        cout<<"s2"<<endl;
         file.close();
 }
 
@@ -24,22 +31,31 @@ int main() {
         cout << "no file with this name" << endl;
     else
     {
-        
         file >> range0 >>range1;
-        cout<<range0<<endl;
     }
     file.close();
 
+    for (int i=range0;i<range1;i++){
+        totalNums++;
+        if(isPrime(i)){
+            numOfPrimes++;
+            primeList.push_back(i);
+        }
+    }  
     /*
-
+    // floor((range 1 - range 0 )/T) 
     */
 
     
     //all threads are done,(join) 
     
     //print the total number of prime numbers found (STDOUT)
+    cout<<"numOfPrime="<<numOfPrimes<<", totalNums="<<totalNums<<endl;
 
     //print all these numbers. (out.txt)
-
+    write("The prime numbers are:");
+    for(auto i=primeList.begin();i!=primeList.end();i++){
+        write(to_string(*i));
+    }
     return 0;
 }
