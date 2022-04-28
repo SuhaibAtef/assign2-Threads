542ad39809ec5cf230390604550e527b071d47b6
Suhaib Atef <suhaibatef20@gmail.com>
2022-04-28 16:58:49 +0300

    defualt commit

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+// numOfPrimes&&TotalNums&&PrimeList
+/*
+
+When any of the threads starts executing, it will print its number (0 to T-1), and then the range of
+    numbers that it is operating on. 
+    
+*/
+
+int main() {
+    
+
+    /*
+    
+    */
+
+    
+    //all threads are done,(join) 
+    
+    //print the total number of prime numbers found (STDOUT)
+
+    //print all these numbers. (out.txt)
+
+    return 0;
+}
