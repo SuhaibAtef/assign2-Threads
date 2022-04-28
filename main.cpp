1c87a9ea63e8a3eff8b585b8db3df32f43c71aff
Suhaib Atef <suhaibatef20@gmail.com>
2022-04-28 17:08:52 +0300

    added-read/write for files

--- main.cpp
+++ main.cpp
@@ -8,12 +8,30 @@ When any of the threads starts executing, it will print its number (0 to T-1), a
     numbers that it is operating on. 
     
 */
+void write(string s) {
+        fstream file;
+        file.open("out.txt", ios::out);
+        file << s << endl;
+        cout<<"s2"<<endl;
+        file.close();
+}
 
 int main() {
-    
+    int range0,range1;
+    fstream file;
+    file.open("in.txt");
+    if (!file)
+        cout << "no file with this name" << endl;
+    else
+    {
+        
+        file >> range0 >>range1;
+        cout<<range0<<endl;
+    }
+    file.close();
 
     /*
-    
+
     */
 
     
